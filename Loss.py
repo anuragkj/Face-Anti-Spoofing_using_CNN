91b66b44fb687b0fc1395219a1c871d8bc4c4041
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-05-01 00:02:37 +0400

    Update Loss.py

--- Loss.py
+++ Loss.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-#Ground truth mask is the actual mask, like what is expected from our mask, the difference bw the two give pixel wise loss
+# Ground truth mask is the actual mask, like what is expected from our mask, the difference bw the two give pixel wise loss
 # pixel loss is used to optimize the model to produce more accurate masks, FOR MASKS 
 # while binary loss is used to optimize the model to produce more accurate binary labels. FOR LABELS
 
