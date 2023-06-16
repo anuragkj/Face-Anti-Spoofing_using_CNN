87d837fa55916c05ac47d54425803408509bc67e
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-06-17 00:12:31 +0530

    Update mtcnn_image.py

--- MTCNN_Face_Extraction/mtcnn_image.py
+++ MTCNN_Face_Extraction/mtcnn_image.py
@@ -2,13 +2,15 @@ import cv2
 from mtcnn.mtcnn import MTCNN
 detector = MTCNN()
 
-img=cv2.imread("resources/test2.jpeg")
+img=cv2.imread("C:/Users/anura/Documents/Github/Face-Anti-Spoofing_using_CNN/MTCNN_Face_Extraction/resources/test4.jpeg")
 location = detector.detect_faces(img)
+print(location)
 if len(location) > 0:
     for face in location:
         x, y, width, height = face['box']
+        print(x,y)
 
 cropped_image = img[y:y+height, x:x+width]
 # cropped_image = cv2.resize(cropped_image, (224, 224))
-cv2.imwrite("resources/Output2.jpg",cropped_image)
+cv2.imwrite(("C:/Users/anura/Documents/Github/Face-Anti-Spoofing_using_CNN/MTCNN_Face_Extraction/resources/Output4.jpeg"),cropped_image)
 print("The Image was successfully saved")
